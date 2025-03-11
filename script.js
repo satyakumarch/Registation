2e4c9a55f4a0db7c14a1dfc4c5208b4d68fc086e
Satyakumarch <satyakumarchaudhary603@gmail.com>
2025-03-11 19:15:07 +0530

    added the neww feature on the javascript file

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.getElementById('registrationForm').onsubmit = function(event) {
     event.preventDefault(); // Prevent the default form submission
     alert("Form submitted successfully!");
     // Here you can add your form submission logic (e.g., AJAX call)
+    location.reload(); // Refresh the form page
 };
 
 document.querySelector('input[type="reset"]').onclick = function(event) {
